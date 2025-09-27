56c0df875282b20c882509bdbdc9fbbfe2cbf2b2
Pavel Malai <pavel.malai@commify.com>
2025-09-27 16:49:19 +0300

    Convert translation files to nested JSON format and initial project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SimleLocalizePOC';
+  
+  languages = [
+    { code: 'en-gb', name: 'English (UK)' },
+    { code: 'es-es', name: 'Español' },
+    { code: 'fr-fr', name: 'Français' },
+    { code: 'de-de', name: 'Deutsch' }
+  ];
+  
+  currentLanguage = 'en-gb';
+
+  constructor(private translate: TranslateService) {}
+
+  ngOnInit() {
+    this.translate.setDefaultLang('en-gb');
+    this.translate.use('en-gb');
+  }
+
+  switchLanguage(language: string) {
+    this.currentLanguage = language;
+    this.translate.use(language);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTranslateHttpLoader({
+      prefix: './assets/i18n/',
+      suffix: '.json'
+    }),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'en-gb'
+      })
+    )
+  ]
 };
